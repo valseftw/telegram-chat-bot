5ee5035cf84b1546c13babb0f7eae85efdff2999
Caner Artuç <48654905+valseftw@users.noreply.github.com>
2024-11-26 02:59:51 +0300

    Update index.js
    *Update*
    Add mongodb
    Create model
    Add data
    Add args

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ const token = 'bottokenid'; // Your bot token id
 
 const bot = new Telegraf(token); // Create a new Telegraf instance
 
+const mongoose = require('mongoose'); // Import the mongoose module
+const Schema = mongoose.Schema; // Create a new mongoose schema
+
+mongoose.connect('mongoose_connect_url',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, () => {
+        console.log("Database bağlandı")
+    }
+
+); // Connect to the database
+
+const userSchema = new Schema({ 
+    name: String,
+    age: Number
+}); // Create a new schema
+ 
+const User = mongoose.model('User', userSchema); // Create a new model
+
+
 bot.start((ctx) => {
 
 }); // Start the bot
@@ -30,5 +51,22 @@ bot.launch({
 bot.on('message', async (ctx) => { // When the bot receives a message
 
     ctx.reply(`Hello, ${ctx.message.from.first_name}! `); // Reply to the user
+
+
+    const args = ctx.message.text.split(' '); // Split the message into arguments
+    if (args[0] === '/register') { // If the first argument is '/register'
+        const age = args[1]; // Get the age
+        const name = args[2]; // Get the name
+        // If the user doesn't enter a name
+        if (!age) return ctx.reply('Please enter your age!'); // If the user doesn't enter an age
+        if (isNaN(age)) return ctx.reply('Please enter a valid age!'); // If the user enters an invalid age
+        if (!name) return ctx.reply('Please enter your name!'); // If the user doesn't enter an age
+        if (isNaN(name)) return ctx.reply('Please enter a valid name!'); // If the user enters an invalid age
+
+
+        const user = new User({ name: args[2], age: age }); // Create a new user
+        await user.save(); // Save the user to the database
+        ctx.reply('User saved successfully!'); // Reply to the user
+    }
 
 });
